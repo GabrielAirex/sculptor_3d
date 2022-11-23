b9d3ea161dda6180bd2b73ea2bef77d1a31431ef
Gabriel Aires <88017824+GabrielAirex@users.noreply.github.com>
2022-11-23 08:04:46 -0300

    Delete parte_1 directory

--- parte_1/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['sculptor_0',['Sculptor',['../classSculptor.html',1,'Sculptor'],['../classSculptor.html#a014e3ef5517bf0e9d9e14486b6ac6433',1,'Sculptor::Sculptor(int _nx, int _ny, int _nz)']]],
-  ['setcolor_1',['setColor',['../classSculptor.html#af1d69da01379874b0dfd6454787cb562',1,'Sculptor']]]
-];
