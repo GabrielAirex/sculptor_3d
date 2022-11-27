96c71fadafff12d679d13297763bcb716e63f95a
Gabriel Aires <88017824+GabrielAirex@users.noreply.github.com>
2022-11-27 20:42:36 -0300

    Delete parte_2/html directory

--- parte_2/html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['interpretador_67',['Interpretador',['../class_interpretador.html',1,'']]]
-];
--- parte_2/html/search/files_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['sculptor_2ecpp_95',['sculptor.cpp',['../sculptor_8cpp.html',1,'']]],
-  ['sculptor_2eh_96',['sculptor.h',['../sculptor_8h.html',1,'']]]
-];
